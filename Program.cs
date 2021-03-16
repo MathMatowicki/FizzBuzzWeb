96a9519636797681fde05e5f79c91be6d020154f
mathmatowicki <matowicki@gmail.com>
2021-03-16 13:05:12 +0100

    Change project name to FizzBuzzWeb

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PS3
+namespace FizzBuzzWeb
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace PS3
+namespace FizzBuzzWeb
 {
     public class Startup
     {
