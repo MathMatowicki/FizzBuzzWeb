476c4e762d239c853dddbb1e3dad577cef5b0b79
mathmatowicki <matowicki@gmail.com>
2021-04-12 18:03:40 +0200

    Simple changes

--- Data/FizzBuzzContext.cs
+++ Data/FizzBuzzContext.cs
@@ -5,6 +5,7 @@ namespace Fizz.Data
 {
     public class FizzBuzzContext : DbContext
     {
+        public FizzBuzzContext(DbContextOptions<FizzBuzzContext> options) : base(options) { }
         public DbSet<FizzBuzz> FizzBuzz { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
            => options.UseSqlite(@"Data Source=C:\Users\Jan\OneDrive\Studia\SemestrVI\.NET\PS3,4\Fizz\fizzbuzz.db");
